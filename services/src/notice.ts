db0c0bffd51f365d46156c34a18b4f10574e508d
Gabriel Barros <gbbabarros@gmail.com>
2023-06-25 11:35:51 -0300

    Accept new json format

--- services/src/notice.ts
+++ services/src/notice.ts
@@ -4,6 +4,8 @@ import {
     NoticeDocument,
 } from "../generated-src/graphql";
 
+import {ethers} from "ethers";
+
 /**
  * Queries a GraphQL server looking for a specific notice
  * @param url URL of the GraphQL server
@@ -27,3 +29,28 @@ export const getNotice = async (url: string, id: string): Promise<Notice> => {
         throw new Error(error?.message);
     }
 };
+
+export const parsePayload = (payload: string) => {
+    const data = JSON.parse(ethers.toUtf8String(payload));
+
+    const parsedData: {
+        "media_cid": string,
+        "descriptor_rawdata": string,
+        "descriptor_data": {
+            "name": string,
+            "description": string,
+            "image": string,
+            "attributes": Array<{
+                "trait_type": string,
+                "value": string
+            }>
+        },
+        "descriptor_cid": string,
+    } = {
+        "media_cid": data.media_cid,
+        "descriptor_rawdata": data.descriptor_data,
+        "descriptor_data": JSON.parse(data.descriptor_data),
+        "descriptor_cid": data.descriptor_cid,
+    };
+    return parsedData;
+}
